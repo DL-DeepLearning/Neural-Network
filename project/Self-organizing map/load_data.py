98521ecb1b9fc44264b4118b681ed81f2e013370
teinhonglo <teinhonglo@gmail.com>
2017-06-04 21:56:58 +0800

    split

--- project/Self-organizing map/load_data.py
+++ project/Self-organizing map/load_data.py
@@ -1,11 +1,8 @@
 import numpy as np
-import som
 import re
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
-classify = []
 non_repeat_answer = []
+classify = []
 features = []
 ''' load data'''
 with open("iris.data.txt") as file:
@@ -20,31 +17,3 @@ with open("iris.data.txt") as file:
 
 features = np.array(features, dtype = np.float32)
 
-''' som '''			
-height = 100
-width = 150
-my_som = som.SOM(height, width, features)
-
-print "Train..."
-my_som.train(10)
-
-print "Inference..."
-Y = []
-for f in features:
-	graph = my_som.inference(f, 1)
-	[[x], [y]] = np.where(graph == 1)
-	Y.append([x, y])
-Y = np.array(Y, dtype = np.float32)
-
-print "visualize..."	
-plt.figure(8)
-colors = cm.rainbow(np.linspace(0, 1, len(non_repeat_answer)))
-for idx, coor in enumerate(Y):
-	c = colors[non_repeat_answer.index(classify[idx])]
-	x, y = coor[0], coor[1]
-	plt.scatter(x, y, color = c)
-	
-for label, x, y in zip(classify, Y[:, 0], Y[:, 1]):
-	plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
-	
-plt.show()	

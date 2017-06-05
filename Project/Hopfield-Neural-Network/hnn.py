430ea0aee2f41f374de6659be1f79470b8917f3a
teinhonglo <teinhonglo@gmail.com>
2017-06-06 01:14:11 +0800

    test data

--- Project/Hopfield-Neural-Network/hnn.py
+++ Project/Hopfield-Neural-Network/hnn.py
@@ -13,24 +13,28 @@ class HNN:
 		mat_len = X.shape[1]
 		W = self.weights
 		
+		# Train weight
 		for x in X:
 			W += x.transpose() * x
-
+			
+		# Set the diagonal of weights to zero
 		for i, j in zip(xrange(mat_len), xrange(mat_len)):
 			W[i, j] = 0
 		
 		self.threashold = -0.5 * W.sum(axis = 1)
 		self.weights = W
+		print W
 
 		
 	def inference(self, input_data):
 		W = self.weights
 		theata = self.threashold
 		prev_Y = np.matrix(input_data).transpose()
+		# binary mask
 		vec_len = input_data.shape[0]
 		positive = np.matrix(np.zeros((vec_len, 1)) + 1)
 		negative = np.matrix(np.zeros((vec_len, 1)) - 1)
-		
+		# interations
 		while True:
 			R =  W * prev_Y + theata
 			Y = np.multiply((R > 0), positive) + np.multiply((R == 0) , prev_Y) + np.multiply((R < 0), negative)
@@ -42,11 +46,15 @@ class HNN:
 		return Y.reshape(-1)
 		
 if __name__ == "__main__":
-	input_data = np.random.randint(2, size=(2, 4)) * 2 - 1
+	input_data = np.array([	[ 1, -1,  1, -1,  1, -1],
+							[-1,  1, -1,  1, -1,  1],
+							[ 1,  1,  1,  1,  1,  1],
+							[-1, -1, -1, -1, -1, -1]
+							])
 	test_data = np.random.randint(2, size=(2, 4)) * 2 - 1
 	Hopfield = HNN(input_data)
-	print input_data[0]
-	print Hopfield.inference(input_data[0])
+	print np.array([1, 1, 1, -1, 1, -1])
+	print Hopfield.inference(np.array([1, 1, 1, -1, 1, -1]))
 
 	
 	
